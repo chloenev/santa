e76e1eab840dbfc8a3ccbdd60c077bbefc6e063d
chloenev <chloe.nevin@flemingcollege.ca>
2023-11-30 13:23:25 -0500

    change file name and add comments

---
+++ DRAFT_Santa.py
@@ -0,0 +1,13 @@
+# Calculating Time & Distance for Santa's Trip from North Pole to Your City! 
+# PSP Group Project 2023
+# Section 60, Group 3
+# Authors: Andie Herstek and Chloe Nevin 
+# Purpose:
+# Description:
+# Assumptions:
+# Planned for limitations: 
+# Special cases/known problems:
+# Inputs:
+# Outputs:
+# References:
+# Contributions: 
--- DistanceCalc.py
+++
@@ -1,4 +0,0 @@
-# Just practicing right now
-northPole = "Finland"
-northPoleLat = 45.9876
-northPoleLong = -15.6872
