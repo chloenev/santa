dfdd4044b7f1042ff80494fa1e8d52bf20b3d015
aherstek <146446987+aherstek@users.noreply.github.com>
2023-12-02 16:14:33 -0700

    Update arcpysanta.py

--- arcpysanta.py
+++ arcpysanta.py
@@ -1,9 +1,13 @@
+# description: create XY layer and export to layer file
+
 # import modules
 import arcpy
+import csv
 from arcpy import env
 
 # environment settings
 env.workspace = "C:\_GEOM67\SantaTracking\SantaPath"
+
 
 try:
 
@@ -12,7 +16,7 @@ try:
     table = "CADcities.csv"
     x_coords = "Y"
     y_coords = "X"
-    out_layer = "cities1_layer"
+    out_layer = "cities2_layer"
     saved_layer = "C:\_GEOM67\SantaTracking\SantaPath\santacities.lyr"
 
     # set spatial reference
@@ -29,9 +33,23 @@ except:
     count = arcpy.GetMessageCount()
     print (arcpy.GetMessage(count-1))
 
+# description: plotting a line between Santa's workshop and cities across Canada
+
 # set variables
 input_table = "C:\_GEOM67\SantaTracking\SantaPath\cities.csv"
-out_lines = "C:\_GEOM67\SantaTracking\SantaPath\SantaPath.gdb\XYlines"
+out_lines = "C:\_GEOM67\SantaTracking\SantaPath\SantaPath.gdb\XYcoordlines"
 
-# XY to line
-arcpy.XYToLine_management(input_table, out_lines, "X_S", "Y_S", "X_E", "Y_E","GEODESIC")     
+# run XY to line
+arcpy.XYToLine_management(input_table, out_lines, "X_S", "Y_S", "X_E", "Y_E","GEODESIC", "Name")      
+
+
+# description: script reads coordinates in csv format and prints list of pairs
+
+import csv
+
+print("Jot down your city's accompanying X,Y coordinates! (X_E and Y_E). You will use this to input into our script tool - SantasPathScript - to see his route on the globe!")
+
+with open ("C:\_GEOM67\SantaTracking\SantaPath\cities.csv") as santacities:
+     csvReader = csv.reader(santacities)
+     for row in csvReader:
+          print(row)
